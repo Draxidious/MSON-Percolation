09afffdade35538512142be020a63e803002b15e
Jordan Muniz <munizj21@trinityprep.org>
2020-01-26 22:24:41 -0500

    feat: Implement Stats

--- src/main/java/PercolationStats.java
+++ src/main/java/PercolationStats.java
@@ -1,3 +1,8 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+import java.util.Random;
+
 /******************************************************************************
  *  Name:    Kevin Wayne
  *  Login:   wayne
@@ -6,7 +11,7 @@
  *  Partner Name:    N/A
  *  Partner Login:   N/A
  *  Partner Precept: N/A
- * 
+ *
  *  Compilation:  javac-algs4 Percolation.java
  *  Execution:    java-algs4 Percolation
  *  Dependencies: StdIn.java StdRandom.java WeightedQuickUnionUF.java
@@ -14,31 +19,67 @@
  *  Description:  Modeling Percolation like a boss. woot. woot.
  ******************************************************************************/
 public class PercolationStats {
-   public PercolationStats(int n, int trials) {
-     // TODO: perform trials independent experiments on an n-by-n grid
-       for(int i = 0; i<trials;i++)
-       {
+    private double[] thresholds;
+    private int T;
+    private double mean = Integer.MIN_VALUE;
+    private double dev = Integer.MIN_VALUE;
 
-       }
-   }
-   public double mean() {
-     // TODO: calculate sample mean of percolation threshold
-     return 0.0;
-   }
-   public double stddev() {
-     // TODO: calculate sample standard deviation of percolation threshold
-     return 0.0;
-   }
-   public double confidenceLo() {
-     // TODO: return low  endpoint of 95% confidence interval
-     return 0.0;
-   }
-   public double confidenceHi() {
-     // TODO: return high endpoint of 95% confidence interval
-     return 0.0;
-   }
+    public PercolationStats(int n, int trials) {
+        // TODO: perform trials independent experiments on an n-by-n grid
 
-   public static void main(String[] args) {
-     // test client (described at http://coursera.cs.princeton.edu/algs4/assignments/percolation.html)
-   }
+        thresholds = new double[trials];
+        T = trials;
+        for (int i = 0; i < trials; i++) {
+            Percolation perc = new Percolation(n);
+            while (!perc.percolates()) {
+                int newx = StdRandom.uniform(1, n);
+                int newy = StdRandom.uniform(1, n);
+                perc.open(newx, newy);
+            }
+            thresholds[i] = perc.numberOfOpenSites() / n;
+
+        }
+    }
+
+    public double mean() {
+        // TODO: calculate sample mean of percolation threshold
+        if (mean == Integer.MIN_VALUE) {
+            mean = StdStats.mean(thresholds);
+        }
+        return mean;
+    }
+
+    public double stddev() {
+        // TODO: calculate sample standard deviation of percolation threshold
+        if (dev == Integer.MIN_VALUE) {
+            dev = StdStats.mean(thresholds);
+        }
+        return dev;
+    }
+
+    public double confidenceLo() {
+        // TODO: return low  endpoint of 95% confidence interval
+        if (mean == Integer.MIN_VALUE) {
+            mean = StdStats.mean(thresholds);
+        }
+        if (dev == Integer.MIN_VALUE) {
+            dev = StdStats.mean(thresholds);
+        }
+        return mean - ((1.96 * dev) / Math.sqrt(T));
+    }
+
+    public double confidenceHi() {
+        // TODO: return high endpoint of 95% confidence interval
+        if (mean == Integer.MIN_VALUE) {
+            mean = StdStats.mean(thresholds);
+        }
+        if (dev == Integer.MIN_VALUE) {
+            dev = StdStats.mean(thresholds);
+        }
+        return mean + ((1.96 * dev) / Math.sqrt(T));
+    }
+
+    public static void main(String[] args) {
+        // test client (described at http://coursera.cs.princeton.edu/algs4/assignments/percolation.html)
+    }
 }
